edfe662e836b7a9669d3ae8561df10c51d26921f
JieFeng <fengjie1102@gmail.com>
2019-02-11 00:23:06 +0800

    添加读取配置文件的类

--- src/core/plusplusi.cpp
+++ src/core/plusplusi.cpp
@@ -1,12 +1,23 @@
-#include <sys/epoll.h>
 #include <iostream>
-#include <arpa/inet.h>
-
-using namespace std;
+#include "plusplusi_config.h"
 
 int main()
 {
-	cout << "Hello World!" << endl;
+	int port;
+	std::string ipAddress;
+	std::string username;
+	std::string password;
+	const std::string ConfigFile = "../conf/config.txt";
+	Config configSettings(ConfigFile);
+
+	port = configSettings.Read("port", 0);
+	ipAddress = configSettings.Read("ipAddress", ipAddress);
+	username = configSettings.Read("username", username);
+	password = configSettings.Read("password", password);
+	std::cout << "port:" << port << std::endl;
+	std::cout << "ipAddress:" << ipAddress << std::endl;
+	std::cout << "username:" << username << std::endl;
+	std::cout << "password:" << password << std::endl;
 
 	return 0;
 }
